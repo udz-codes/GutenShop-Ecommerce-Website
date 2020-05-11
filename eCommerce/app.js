80ba8982e3983265d5e45aa69e4e91cae6bb7d68
Ujjwaldeep <ujjwal2699singh@gmail.com>
2020-05-11 16:07:41 +0530

    Final Chnages

--- eCommerce/app.js
+++ eCommerce/app.js
@@ -20,6 +20,7 @@ const orderRoutes = require('./routes/order');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
